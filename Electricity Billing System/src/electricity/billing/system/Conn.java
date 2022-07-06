bb5d22ec7f4787a9540b8dcd1882e40e5f4062e4
priyabrata77 <107979420+priyabrata77@users.noreply.github.com>
2022-07-06 23:24:33 +0530

    Add files via upload

---
+++ Electricity Billing System/src/electricity/billing/system/Conn.java
@@ -0,0 +1,18 @@
+
+package electricity.billing.system;
+import java.sql.*;
+
+public class Conn {
+  Connection c;
+  Statement s;  
+    Conn() {
+        try{
+            c = DriverManager.getConnection("jdbc:mysql:///evs", "root", "biplab@123"); 
+            s = c.createStatement();
+        }catch (Exception e){
+             e.printStackTrace(); 
+        }
+      
+    }
+    
+}
