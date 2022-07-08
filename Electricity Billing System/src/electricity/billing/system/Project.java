d4138dc6acf5d4d3a00edc300026985848348cae
biplabgithub <108607451+biplabgithub@users.noreply.github.com>
2022-07-09 01:02:09 +0530

    Add files via upload

--- Electricity Billing System/src/electricity/billing/system/Project.java
+++ Electricity Billing System/src/electricity/billing/system/Project.java
@@ -4,7 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Project extends JFrame {
+
+public class Project extends JFrame implements ActionListener {
     
     Project(){
         setExtendedState(JFrame.MAXIMIZED_BOTH);
@@ -29,6 +30,7 @@ public class Project extends JFrame {
         Image image1 = icon1.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
         newcustomer.setIcon(new ImageIcon(image1));
         newcustomer.setMnemonic('D');
+        newcustomer.addActionListener(this);
         newcustomer.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, ActionEvent.CTRL_MASK));
         
         master.add(newcustomer);
@@ -39,7 +41,8 @@ public class Project extends JFrame {
         ImageIcon icon2 = new ImageIcon(ClassLoader.getSystemResource("icon/icon2.png"));
         Image image2 = icon2.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
         customerdetails.setIcon(new ImageIcon(image2));
-        customerdetails.setMnemonic('D');
+        customerdetails.setMnemonic('M');
+        customerdetails.addActionListener(this);
         customerdetails.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_M, ActionEvent.CTRL_MASK));
         
         master.add(customerdetails);
@@ -50,7 +53,8 @@ public class Project extends JFrame {
         ImageIcon icon3 = new ImageIcon(ClassLoader.getSystemResource("icon/icon3.png"));
         Image image3 = icon3.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
         depositdetails.setIcon(new ImageIcon(image3));
-        depositdetails.setMnemonic('D');
+        depositdetails.setMnemonic('N');
+        depositdetails.addActionListener(this);
         depositdetails.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
         
         master.add(depositdetails);
@@ -62,6 +66,7 @@ public class Project extends JFrame {
         Image image4 = icon4.getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT);
         calculatebill.setIcon(new ImageIcon(image4));
         calculatebill.setMnemonic('B');
+        calculatebill.addActionListener(this);
         calculatebill.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_B, ActionEvent.CTRL_MASK));
         
         master.add(calculatebill);
@@ -183,6 +188,20 @@ public class Project extends JFrame {
         setVisible(true);
         
     }
+    
+    public void actionPerformed(ActionEvent ae) {
+        String msg = ae.getActionCommand();
+        if (msg.equals("New Customer")) {
+            new NewCustomer();
+        } else if (msg.equals("Customer Details")) {
+            
+        } else if (msg.equals("Deposit Details")) {
+            
+        } else if (msg.equals("Calculate Bill")) {
+            new CalculateBill();
+        }
+    }
+    
     public static void main(String[] args) {
         new Project();
     }
